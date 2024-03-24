a099af8314b00591377f6f770407d46f0232790f
SergeyAli <108223744+SergeyAli@users.noreply.github.com>
2024-03-24 16:12:10 +0700

    Update Seminar_SQL_6_1.sql
    Задача №1
    Создайте функцию, которая принимает кол-во сек и формат их в кол-во дней часов.

--- Seminar_SQL_6_1.sql
+++ Seminar_SQL_6_1.sql
@@ -1,1 +1,35 @@
+/*
+Задача №1
+Создайте функцию, которая принимает кол-во сек и формат их в кол-во дней часов. 
+Пример: 
+123456 ->'1 days 10 hours 17 minutes 36 seconds '
+*/
 
+DROP FUNCTION IF EXISTS seconds_to_DT;
+DELIMITER $$
+CREATE PROCEDURE seconds_to_DT(seconds INT)
+BEGIN
+    DECLARE days INT default 0;
+    DECLARE hours INT default 0;
+    DECLARE minutes INT default 0;
+
+    SET days = seconds / 86400;
+    SET seconds = seconds % 86400;
+  
+    SET hours = seconds / 3600;
+    SET seconds = seconds % 3600;
+
+    SET minutes = seconds / 60;
+    SET seconds = seconds % 60;
+
+SELECT CONCAT
+(
+  days, ' days ',
+  hours, ' hours ',
+  minutes, ' minutes ',
+  seconds, ' seconds'
+) AS date_time;
+END $$
+DELIMITER ;
+
+CALL seconds_to_DT(123456);
